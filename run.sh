ec06265bbf65711cb9140c926fce52fc9e4308d3
wip727 <wip727@gmail.com>
2015-11-06 02:23:29 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
+# ChangQing Wang
+# Twitter cleaner and hashtag analyzer
+# Please read README.md
 python ./src/tweets_cleaned.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
 python ./src/average_degree.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
 
